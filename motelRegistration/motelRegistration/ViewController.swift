879bd0384cad988240890f26199d372d5cdd3d42
Vidhur Savyasachin <vidhursachin@gmail.com>
2020-02-22 00:40:06 -0500

    First commit
    Check it out

---
+++ motelRegistration/motelRegistration/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  motelRegistration
+//
+//  Created by Vidhur Savyasachin on 2/22/20.
+//  Copyright © 2020 Vidhur Savyasachin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("shalini likes a finger up her ass. ")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
